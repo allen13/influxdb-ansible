8fd48eba430ab02a1e4a9fbbe66d2bb1a97402c5
Tim Allen <tiallen@softlayer.com>
2015-09-11 15:04:07 -0500

    Setup influxdb and telegraf to run clustered without need for consul. Add grafana role.

--- vagrant/shared.rb
+++ vagrant/shared.rb
@@ -16,10 +16,10 @@ def create_vm(config, options = {})
     config.vm.box = "bento/centos-7.1"
     config.vm.hostname = vm_name
 
-    public_ip = "10.0.0.10#{id}"
+    public_ip = "10.0.168.10#{id}"
     config.vm.network :private_network, ip: public_ip, netmask: "255.255.255.0"
 
-    private_ip = "10.0.1.10#{id}"
+    private_ip = "10.0.169.10#{id}"
     config.vm.network :private_network, ip: private_ip, netmask: "255.255.255.0"
 
     config.vm.provider :virtualbox do |vb|
